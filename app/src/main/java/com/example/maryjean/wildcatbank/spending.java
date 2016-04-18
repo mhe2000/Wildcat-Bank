8ad3150af321279fc013b815aa147e94cef26637
Maryjean <mhe2000@wildcats.unh.edu>
2016-04-18 19:22:32 -0400

    trying to commit changes

---
+++ app/src/main/java/com/example/maryjean/wildcatbank/spending.java
@@ -0,0 +1,23 @@
+package com.example.maryjean.wildcatbank;
+
+/**
+ * Created by Maryjean on 4/13/2016.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hp1 on 21-01-2015.
+ */
+public class spending extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.spending,container,false);
+        return v;
+    }
+}
