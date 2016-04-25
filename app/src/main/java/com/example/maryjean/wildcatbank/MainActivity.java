3d55a728417f1fdf5b7700469bf0f99218fa15d6
Maryjean <mhe2000@wildcats.unh.edu>
2016-04-25 09:59:34 -0400

    commit for all my errors

--- app/src/main/java/com/example/maryjean/wildcatbank/MainActivity.java
+++ app/src/main/java/com/example/maryjean/wildcatbank/MainActivity.java
@@ -22,6 +22,7 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+
 public class MainActivity extends AppCompatActivity {
 
     /**
--- app/src/main/java/com/example/maryjean/wildcatbank/opening_page.java
+++ app/src/main/java/com/example/maryjean/wildcatbank/opening_page.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 /**
  * Created by hp1 on 21-01-2015.
  */
