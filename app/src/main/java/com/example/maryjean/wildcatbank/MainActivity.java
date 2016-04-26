3d630de808e283df62970070e75bea75e5be3a8f
Bonnie <bmr28@wildcats.unh.edu>
2016-04-26 19:01:48 -0400

    camera

--- app/src/main/java/com/example/maryjean/wildcatbank/MainActivity.java
+++ app/src/main/java/com/example/maryjean/wildcatbank/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
